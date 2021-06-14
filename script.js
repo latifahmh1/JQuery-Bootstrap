5151df7977828b2bad7e8d38b10b1b7b2ca77e6c
Latifah Humphreys <latifahmh1@gmail.com>
2021-06-14 19:32:21 -0400

    more button margin edits, new box styles for click events added

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ $( document ).ready(function() {
 
     $( ".noLeftP" ).click(function() {
         console.log("change btn 1 clicked");
-        $(".box1").addClass("newbox1");
+        $(".firstbox").addClass("newfirstbox");
     });
 
     $( ".change2" ).click(function() {
         console.log("change btn 2 clicked");
-        $(".box2").css({"background":"red","border":"2px solid blue"});
+        $(".secondbox").css({".newsecondbox":"red","border":"2px solid blue"});
     });
-
+    $( ".noLeftP" ).click(function() {
+        console.log("change btn 1 clicked");
+        $(".firstbox").addClass("newfirstbox");
+    });
+    
     $( ".change3" ).click(function() {
         console.log("change btn 3 clicked");
     });
