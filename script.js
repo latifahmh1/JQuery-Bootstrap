dd823b0c18325fc44505bf8fba6697d480ca74c4
Latifah Humphreys <latifahmh1@gmail.com>
2021-06-14 23:52:55 -0400

    some javascript code added and change all edits

--- script.js
+++ script.js
@@ -2,20 +2,33 @@ $( document ).ready(function() {
 
     console.log( "ready!" );
 
-    $( ".noLeftP" ).click(function() {
-        console.log("change btn 1 clicked");
-        $(".firstbox").addClass("newfirstbox");
+    $('.noLeftP').on('click', function(){
+        console.log('change btn 1 clicked');
+        $('.firstbox').addClass('newfirstbox');
     });
+});
 
     $( ".change2" ).click(function() {
         console.log("change btn 2 clicked");
-        $(".secondbox").css({".newsecondbox":"red","border":"2px solid blue"});
+        $(".firstbox").addClass("newsecondbox");
     });
-    $( ".noLeftP" ).click(function() {
-        console.log("change btn 1 clicked");
-        $(".firstbox").addClass("newfirstbox");
+
+    $( ".change3" ).click(function() {
+        console.log("change btn 3 clicked");
+        $(".firstbox").addClass("newthirdbox");
     });
-    
+
+    $( ".noRightP" ).click(function() {
+        console.log("change all btn clicked");
+        $(".firstbox").css({".newsecondbox":"black","border":"6px solid gold"});
+        $(".newfirstbox").addClass("changeallbox");
+        $(".secondtbox").addClass("changeallbox");
+        $(".newsecondbox").addClass("changeallbox");
+        $(".thirdbox").addClass("changeallbox");
+        $(".newthirdbox").addClass("changeallbox");
+    });
+   
+
     $( ".change3" ).click(function() {
         console.log("change btn 3 clicked");
     });
